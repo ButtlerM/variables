7d43a12136e9a49550d76589fcfc29be898d0f46
ButtlerM <morten.buttler@journalism.cuny.edu>
2016-02-15 10:21:06 -0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+var One = 10
+var Two = 5
+
+var addition = One + Two;
+var subtraction = One - Two;
+var multiply = Two * One;
+
+document.getElementById("addition").innerHTML= "adding " +One+ " and " +Two+ " it equals: " + addition;
+document.getElementById("subtraction").innerHTML= "subtracting " +One+ " and " +Two+ " it equals: " + subtraction;
+document.getElementById("multiply").innerHTML= "multiplying " +Two+ " and " +One+ " it equals: " + multiply;
